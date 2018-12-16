af21abde447c1fb607f20d4f6d58a39af1c00ac3
Sean McPherson <seanmcp25@gmail.com>
2018-12-15 22:31:30 -0500

    🚂 Start converting styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import MainNav from './MainNav'
 import Responsive from './common/Responsive'
-import Thumbnail from './common/Thumbnail'
+
+const StyledContainer = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 0;
+
+  @media screen and (max-width: 832px) {
+    flex-direction: column;
+  }
+`
 
 const Header = props => {
   return (
     <header className="Header">
-      <Responsive modifier="header">
-        <Link className="_logo-link" to={'/'}>
-          {props.title}
-        </Link>
-        <MainNav location={props.location} />
+      <Responsive>
+        <StyledContainer>
+          <Link className="_logo-link" to={'/'}>
+            {props.title}
+          </Link>
+          <MainNav location={props.location} />
+        </StyledContainer>
       </Responsive>
     </header>
   )
--- src/components/common/Responsive.js
+++ src/components/common/Responsive.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Responsive = ({ children, modifier }) => {
-  return (
-    <section className={`Responsive ${modifier ? `-${modifier}` : ''}`}>
-      {children}
-    </section>
-  )
+const StyledSection = styled.section`
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
+
+  @media screen and (max-width: 832px) {
+    margin-left: 1rem;
+    margin-right: 1rem;
+    width: initial;
+  }
+`
+
+const Responsive = ({ children }) => {
+  return <StyledSection>{children}</StyledSection>
 }
 
 export default Responsive
