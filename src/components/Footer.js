27afea0b53ce13acf52c82c88f4b3a773911c1a4
Sean McPherson <seanmcp25@gmail.com>
2018-12-29 11:42:40 -0500

    Add styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import THEME from '../styles/theme'
 
 import Responsive from './common/Responsive'
 
+const StyledFooter = styled.footer`
+  background: ${THEME.BACKGROUND_SECONDARY};
+  color: ${THEME.TEXT_SECONDARY};
+  padding: 1rem 0;
+
+  p {
+    margin: 0;
+  }
+`
+
 const Footer = () => {
   return (
-    <footer className="Footer">
-      <Responsive modifier="footer">
+    <StyledFooter>
+      <Responsive justifyContent="space-between" >
         <p className="_attribution">© Sean McPherson 2018</p>
         <p className="_sdg">(SDG)</p>
       </Responsive>
-    </footer>
+    </StyledFooter>
   )
 }
 
