cf722f64b58c9946f592e1cbaeadd3cf69644b22
Sean McPherson <seanmcp25@gmail.com>
2019-01-01 15:14:14 -0500

    💅 Convert to styled-components

--- src/components/common/ArticleCategories.js
+++ src/components/common/ArticleCategories.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import CategoryLink from './CategoryLink'
 
+const StyledSection = styled.section`
+  display: flex;
+  margin: 1rem 0;
+`
+
+const StyledUl = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`
+
+const StyledLi = styled.li`
+  display: flex;
+  // I don't like this, but it overrides
+  // the default li styles that I've used
+  margin: 0 0 0 0.25rem !important;
+`
+
 const ArticleCategories = ({ list }) => {
   return (
-    <section className={'ArticleCategories'}>
-      <span className={'_title'}>Categories:</span>
-      <ul className={'_list'}>
-        {list.map(category => (
-          <li className={'_list-item'} key={category.fieldValue}>
+    <StyledSection>
+      <span>Categories:</span>
+      <StyledUl>
+        {list.map((category, i) => (
+          <StyledLi key={category.fieldValue}>
             <CategoryLink category={category.fieldValue} hideIcon />
-          </li>
+            {i !== list.length - 1 && ','}
+          </StyledLi>
         ))}
-      </ul>
-    </section>
+      </StyledUl>
+    </StyledSection>
   )
 }
 
