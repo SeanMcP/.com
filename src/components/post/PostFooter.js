aed658cc35729e96a4de3ff02a693936846f249a
Sean McPherson <seanmcp25@gmail.com>
2019-01-02 23:15:56 -0500

    💅 Convert to styled-components

--- src/components/post/PostFooter.js
+++ src/components/post/PostFooter.js
@@ -1,34 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import PostDate from './PostDate'
 import CategoryLink from '../common/CategoryLink'
 import TagLinks from '../common/TagLinks'
 import TweetButton from './TweetButton'
 
-const PostFooter = ({
-  articleHref,
-  articleTitle,
-  category,
-  date,
-  modifier,
-  tags
-}) => (
-  <footer className={`PostFooter ${modifier ? `-${modifier}` : ''}`}>
-    {date && <PostDate date={date} className="_post-date" />}
+import THEME from '../../styles/theme'
+
+const StyledFooter = styled.footer`
+  align-items: center;
+  display: flex;
+
+  > *:not(:last-child) {
+    margin-right: 2rem;
+  }
+
+  @media screen and (max-width: ${THEME.SIZE_MOBILE}) {
+    align-items: flex-start;
+    flex-direction: column;
+
+    > *:not(:last-child) {
+      margin: 0 0 1rem;
+    }
+  }
+`
+
+const PostFooter = ({ articleHref, articleTitle, category, date, tags }) => (
+  <StyledFooter>
+    {date && <PostDate date={date} />}
     {category && <CategoryLink category={category} />}
-    <TagLinks modifier={'post-footer'} tags={tags} />
+    <TagLinks tags={tags} />
     {articleHref && articleTitle && (
       <TweetButton articleHref={articleHref} articleTitle={articleTitle} />
     )}
-  </footer>
+  </StyledFooter>
 )
 
 PostFooter.propTypes = {
   articleHref: PropTypes.string,
   articleTitle: PropTypes.string,
+  category: PropTypes.string,
   date: PropTypes.string,
-  modifier: PropTypes.string,
   tags: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
