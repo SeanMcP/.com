d637617f967b326a5c70352a1f06524b10546fbc
Sean McPherson <seanmcp25@gmail.com>
2019-02-13 06:41:02 -0500

    Focus on #main

--- src/components/SkipLink.js
+++ src/components/SkipLink.js
@@ -2,21 +2,33 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 
-const StyledLink = styled(Link)`
-  border: 1px dotted ${p => p.theme.accent};
-  left: -9999px;
+const StyledLink = styled.a`
+  background: ${p => p.theme.primary_dark};
+  color: white !important;
+  font-weight: bold;
   padding: 1rem;
   position: absolute;
+  right: 100%;
   top: 0;
   z-index: 9999;
 
   &:focus {
     left: 0;
+    right: initial;
   }
 `
 
-const SkipLink = () => {
-  return <StyledLink to="#main">Skip to main content</StyledLink>
+class SkipLink extends React.Component {
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      if (location.hash === '#main') {
+        document.getElementById('main').focus()
+      }
+    })
+  }
+  render() {
+    return <StyledLink href="#main">Skip to main content</StyledLink>
+  }
 }
 
 export default SkipLink
