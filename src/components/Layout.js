61f4d6b0d57ea671f32a896e4246fb48df2f3dd6
Sean McPherson <seanmcp25@gmail.com>
2019-04-01 13:10:18 -0400

    Clean up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,33 +1,15 @@
 import React from 'react'
-import styled, { ThemeProvider } from 'styled-components/macro'
+import { ThemeProvider } from 'styled-components/macro'
 
 import '../styles/_index.css'
 import { theme } from '../styles/theme'
 import GlobalStyle from '../styles/styles'
-import Billboard from './Billboard'
 import Header from './Header'
-import Responsive from './common/Responsive'
 import Footer from './Footer'
 import SkipLink from './SkipLink'
 
 import * as S from './Layout.styled'
 import ContentContainer from './ContentContainer'
-const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0;
-  min-height: 100vh;
-`
-
-const StyledResponsive = styled(Responsive)`
-  flex: 1;
-  margin-bottom: 2rem;
-  margin-top: 1rem;
-`
-
-const StyledMain = styled.main`
-  padding: 1rem 0;
-`
 
 const Layout = ({ freeform, location, title, children }) => {
   const Wrapper = freeform ? React.Fragment : ContentContainer
@@ -36,7 +18,7 @@ const Layout = ({ freeform, location, title, children }) => {
       <>
         <GlobalStyle />
         <SkipLink />
-        <Header />
+        <Header home={location.pathname === '/'} />
         <Wrapper>
           <S.Main role="main" id="main">
             {children}
